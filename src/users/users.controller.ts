4bdc479ab3a993473776ace84743575bcbe3379c
kaydence <kaydence880615@gmail.com>
2026-07-04 17:28:20 +0800

    add cookieSession

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -24,14 +25,29 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('whoami')
+  whoAmI(@Session() session: any) {
+    console.log(session);
+    return this.usersService.findOne(session.userId);
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('logout')
+  logout(@Session() session: any) {
+    session.userId = null;
   }
 
   @Get(':id')
