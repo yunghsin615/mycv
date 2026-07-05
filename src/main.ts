79a73f1552d31e7d6dc0c670776d58eaddc8ddd5
kaydence <kaydence880615@gmail.com>
2026-07-05 17:37:45 +0800

    add currentUser(decorator + interceptor) & guard

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     cookieSession({
-      keys: ['qwerqwer'],
+      keys: ['asdfasdf'],
     }),
   );
   app.useGlobalPipes(
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -16,6 +17,9 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -25,10 +29,16 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // @Get('whoami')
+  // whoAmI(@Session() session: any) {
+  //   // return this.usersService.findOne(session.userId);
+  //   return session.currentUser;
+  // }
+
+  @UseGuards(AuthGuard)
   @Get('whoami')
-  whoAmI(@Session() session: any) {
-    console.log(session);
-    return this.usersService.findOne(session.userId);
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signup')
