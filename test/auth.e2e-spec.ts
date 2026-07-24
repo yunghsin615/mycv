31090c842e6e279f3b97591d8ef248e2d4d1753c
kaydence <kaydence880615@gmail.com>
2026-07-24 17:05:03 +0800

    move cookie session & pipe for testing (from main.ts to app.module)

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'aaa1@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'aaa' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
